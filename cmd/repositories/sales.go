42dfd1005dda26e0552e21080c1d028f5d08c3a7
FabioVV <fabio-varela@protonmail.com>
2025-11-17 17:18:15 -0300

    WIP fix email

--- cmd/repositories/sales.go
+++ cmd/repositories/sales.go
@@ -1566,7 +1566,7 @@ func SendQuoteViaEmail(id_user int, id int, email *models.EmailQuote) error {
 	err = mail.SendSystemEmail(*salesOrder.CustomerEmail,
 		fmt.Sprintf("%s: Orçamento/Pedido #%d", *email.Subject, *salesOrder.Id), fmt.Sprintf("%s\n\n Segue o link de acesso do seu orçamento/pedido: %s", *email.Body, *email.Url))
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Could not send email"+err.Error())
+		return echo.NewHTTPError(http.StatusInternalServerError, "Could not send email: "+err.Error())
 	}
 
 	_ = audit.InsertLog(id_user, "/sales/orders/:id/send-quote", "POST", "Quote email sent to customer", "")
